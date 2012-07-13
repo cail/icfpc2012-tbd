20ec4c07fcbf8a0f2195a152790f72f6fe91ddd4
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2012-07-13 23:28:38 +0400

    change map_number to map_name in validate

--- production/game.py
+++ production/game.py
@@ -231,13 +231,13 @@ def play(map):
             
             
 
-def validate(map_number, route):
+def validate(map_name, route):
     '''Validate with my emulator
     
     Follows webvalidator interface.
     Return tuple (score, world).
     '''
-    map = Map.load_file('../data/sample_maps/contest{}.map'.format(map_number))
+    map = Map.load_file('../data/sample_maps/{}.map'.format(map_name))
     
     e = None
     for c in route:
@@ -252,8 +252,7 @@ def validate(map_number, route):
     if e is not None:
         score = e
     else:
-        map.aborted = True
-        score = map.ending()
+        score = map.intermediate_score()
         assert score is not None
         
     return (score, map.get_map_string())
@@ -262,4 +261,4 @@ def validate(map_number, route):
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
-    my = validate(1, 'RRR')
+    my = validate('contest1', 'RRR')
