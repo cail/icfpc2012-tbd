5bb178a171c7ddd291921adfc11cf9834b48c453
Fj <fj.mail@gmail.com>
2012-07-16 14:17:33 +0300

    fixed lookahead.map

--- fj_scratch/test2.py
+++ fj_scratch/test2.py
@@ -1,13 +1,12 @@
-from genetic import GeneticSolver
-import world as world_module
-from world import World
-import time
-
-t_start = time.time()
-timeout = 30
-map_path = '../data/sample_maps/contest10.map'
-world = World.from_file(map_path)
-solver = GeneticSolver(world)
-print solver.solve(timeout)
-
-print 'yo {:0.1f}'.format(world_module.application_counter / (time.time() - t_start))
+s = '''
+##############
+#1**#\ B# *@3#
+#\..   *  .. #
+#@@#   E#\\\ #
+#!.\*  D ##  #
+#  #A W#\#@*W#
+#* \* ###..C\#
+#*..#R! W* 2 #
+#####L########
+'''
+print '%r' % sorted(set(s))
