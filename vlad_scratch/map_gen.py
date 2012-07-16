90308827b89ea02af44a54e4105b03b0d8973c0e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2012-07-16 14:48:03 +0400

    map generator

---
+++ vlad_scratch/map_gen.py
@@ -0,0 +1,63 @@
+import sys
+import random
+
+
+def create_map(width, height):
+    
+    def rand_coords():
+        return random.randrange(width), random.randrange(height)
+    
+    data = {}
+    
+    for x in range(width):
+        for y in range(height):
+            data[x, y] = random.choice(r'   ...#*\@W!ABCDEFGHI')
+     
+    unique = 'RL123456789'
+    for c in unique:
+        while True:
+            xy = rand_coords()
+            if data[xy] not in unique:
+                data[xy] = c
+                break
+    #data[rand_coords()] = 'R'
+   # 
+    #while True:
+    #    c = rand_coords()
+    #    if data[c] != 'R':
+    #        data[c] = 'L'
+    #        break
+    
+    #print data
+    result = ''
+    
+    for y in range(height):
+        for x in range(width):
+            result += data[x, y]
+        result += '\n'
+    result += \
+"""
+Growth 15
+Razors 0
+Water 1
+Flooding 8 
+Waterproof 5
+Trampoline A targets 1
+Trampoline B targets 2
+Trampoline C targets 3
+Trampoline D targets 4
+Trampoline E targets 5
+Trampoline F targets 6
+Trampoline G targets 7
+Trampoline H targets 8
+Trampoline I targets 9"""    
+    return result
+    
+    
+if __name__ == '__main__':
+    random.seed(666)
+    for i in range(10):
+        with open('../data/stress/shit{}.map'.format(i), 'w') as fout:
+            m = create_map(30+10*i, 30+10*i)
+            #print m
+            print>>fout, m
