aa12bd9db905c150658dd570a37b40b34facacc6
Fj <fj.mail@gmail.com>
2012-07-16 12:29:40 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 rm -rf build
 mkdir build
-cp -r production build/src
+cp -r production data build/src
 cp install lifter README PACKAGES-TESTING build/
+cp pypy-1.9-linux.tar.bz2 build/
+(cd build && tar -czf ../icfp-96269702.tgz *)
+cp -f icfp-*.tgz /media/sf_ICFPC
